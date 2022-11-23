b17217ac8e145a59c98e5e6f1f69ae030c89ad69
lincoln munene <lincolnmunene37@gmail.com>
2022-11-23 16:49:53 +0300

    Added public to url

--- resources/views/mycollection.blade.php
+++ resources/views/mycollection.blade.php
@@ -378,23 +378,13 @@
 
             @if($mycollections->count()>0)
 
-            <div class="rn-live-bidding-area rn-section-gapTop" id="u_0_v">
-                <div class="container">
-                    <div class="lightui1">
-                        <div class="lightui1-shimmer">
-
-                        </div>
-                    </div>
-
-                </div>
-
-            </div>
+     
 
             @foreach($mycollections as $mycollection)
             <div data-sal="slide-up" id="mp" style="display:none" data-sal-delay="150" data-sal-duration="800" class="col-5 col-lg-4 col-md-6 col-sm-6 col-12">
                 <div class="product-style-one no-overlay">
                     <div class="card-thumbnail">
-                        <a href="{{url('nfts')}}/{{$mycollection->ref}}"><img src="{{url('nfts')}}/{{$mycollection->img}}" alt="NFT_portfolio"></a>
+                        <a href="{{url('nfts')}}/{{$mycollection->ref}}"><img src="{{url('public/nfts')}}/{{$mycollection->img}}" alt="NFT_portfolio"></a>
                     </div>
                     <div class="product-share-wrapper">
                         <div class="profile-share">
@@ -424,11 +414,9 @@
                     <a href="nft/{{$mycollection->ref}}"><span class="product-name">{{$mycollection->name}}</span></a>
                     <span class="latest-bid">Highest bid 1/20</span>
                     <div class="bid-react-area">
-                        <div class="last-bid">USD {{$mycollection->price}}</div>
+                        <div class="last-bid">USDT {{$mycollection->price}}</div>
                         <div class="react-area">
-                            <svg viewBox="0 0 17 16" fill="none" width="16" height="16" class="sc-bdnxRM sc-hKFxyN kBvkOu">
-                                <path d="M8.2112 14L12.1056 9.69231L14.1853 7.39185C15.2497 6.21455 15.3683 4.46116 14.4723 3.15121V3.15121C13.3207 1.46757 10.9637 1.15351 9.41139 2.47685L8.2112 3.5L6.95566 2.42966C5.40738 1.10976 3.06841 1.3603 1.83482 2.97819V2.97819C0.777858 4.36443 0.885104 6.31329 2.08779 7.57518L8.2112 14Z" stroke="currentColor" stroke-width="2"></path>
-                            </svg>
+                            <img src="{{url('/public/nfts/usdt.png')}}" height="20" width="20">
                             <span class="number">{{$mycollection->likes}}</span>
                         </div>
                     </div>
