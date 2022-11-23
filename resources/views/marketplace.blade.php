cb5821d9eecf20a455d6639dfb1e3d5747c79ffb
lincoln munene <lincolnmunene37@gmail.com>
2022-11-23 16:36:01 +0300

    Added collections shimmer

--- resources/views/marketplace.blade.php
+++ resources/views/marketplace.blade.php
@@ -471,18 +471,7 @@ $nfts = App\Models\Marketplace::all();
  <div class="container">
     <div class="lightui1">
         <div class="lightui1-shimmer">
-            <!-- <div class="_2iwr"></div> -->
-            <!-- <div class="_2iws"></div> -->
-            <!-- <div class="_2iwt"></div>
-            <div class="_2iwu"></div> -->
-            <!-- <div class="_2iwv"></div> -->
-            <!-- <div class="_2iww"></div> -->
-            <!-- <div class="_2iwx"></div>
-            <div class="_2iwy"></div> -->
-            <!-- <div class="_2iwz"></div>
-            <div class="_2iw-"></div> -->
-            <!-- <div class="_2iw_"></div> -->
-            <!-- <div class="_2ix0"></div> -->
+            
         </div>
     </div>
 
