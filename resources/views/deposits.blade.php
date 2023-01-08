4cd9429bea706b05a8af463d6d995b89bb29bab2
lincoln munene <lincolnmunene37@gmail.com>
2023-01-08 22:21:38 +0300

    Fiexed the deposits page issue

--- resources/views/deposits.blade.php
+++ resources/views/deposits.blade.php
@@ -12,8 +12,8 @@
                 <div class="form-wrapper-one registration-area">
                         <h3 class="mb--30">Instructions</h3>
                         <!-- <marquee>Please make sure you transfer assets to that address before clicking the "I have tranfered" Button</marquee> -->
-                        <p>1.Ensure you have the desired amount of usdt in your wallet.</p>
-                        <p>2.Copy the wallet address on the left and send the usdt to it.</p>
+                        <p>1.Ensure you have the desired amount of usdt in your Binance wallet.</p>
+                        <p>2.Copy the wallet address on the right and send the usdt to it using Tron network. Ensure the desired amount is the value of usdt after gas fee.</p>
                         <p>3.Complete the transfer in your exchange and paste the transaction hash.</p>
                         <p>4.Click the complete transfer button on success transfer of assets.</p>
                     </div>
